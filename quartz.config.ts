cd163ca2f6b7ab66b0aaeff66bfd443bc0d096ee
rolirk <rolirk@protonmail.com>
2025-12-07 17:49:06 +0100

    Quartz sync: Dec 7, 2025, 5:49 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,13 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "rolirk",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    analytics: null,
+    locale: "de-DE",
+    baseUrl: "rolirk.github.io/rolirk-site",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
