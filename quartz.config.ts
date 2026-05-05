2c34701619c569f223c367f9228959c8da8fa529
rolirk <rolirk@protonmail.com>
2026-05-05 15:07:09 +0200

    Seitentitel

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "rolirk",
+    pageTitle: "roland kriesling",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
